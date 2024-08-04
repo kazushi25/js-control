70eb95f09c7af51fdde279b765abc8b12ab73368
kazushi25 <oliver25segovia@gmail.com>
2024-08-04 17:43:43 -0400

    primer commit

---
+++ app.js
@@ -0,0 +1,13 @@
+/*
+     DOMContentLoaded sirve para controlar
+     que primero se cargue TODO el documento
+     HTML, por ultimo javascript
+*/
+document.addEventListener&("DOMContentLoaded" , function(){
+    alert("Sacate alv rodrigo")
+});
+
+function hola(nombre, apellido){
+    console.log(nombre, apellido);
+    // codigo reutilizable
+}
